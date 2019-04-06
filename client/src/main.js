35fbc61385ed1b18748a20ea7ccef95344b97cdf
Amanda <aeverett1317@gmail.com>
2019-04-06 14:23:49 -0700

    added vuetify as dependency and added theming

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.config.productionTip = false
+
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.purple,
+    secondary: colors.grey.darken1,
+    accent: colors.shades.black,
+    error: colors.red.accent3
+  }
+})
 
 new Vue({
   router,
