ec072f187ee69ca5849882649bc6c32f92dbee93
Amanda <aeverett1317@gmail.com>
2019-04-06 14:40:52 -0700

    added toolbar and formated app view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <ToolbarComp />
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
+
+<script>
+import ToolbarComp from "@/components/ToolbarComp.vue";
+export default {
+  name: "App",
+  components: {
+    ToolbarComp
+  }
+};
+</script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
