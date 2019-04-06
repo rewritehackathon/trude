8586487122f454ab062d299cc2591a6f558a1b9a
Nancy <butlern0513@gmail.com>
2019-04-06 15:19:22 -0700

    added demo FEMA alert

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout row>
+    <v-flex>
+      <h3>{{this.store.state}}</h3>  
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
