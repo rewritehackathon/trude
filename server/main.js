7eac361277ce11421fd297fff76139c2498e9966
jessicaann <jessicaecottrell@gmail.com>
2019-04-07 11:24:33 -0700

    finished loading common connection profile

--- server/main.js
+++ server/main.js
@@ -5,18 +5,36 @@ const http = require('http');
 
 const port = 3000;
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   // res.end('Hello World!!!!\n');
-// });
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Get request to the homepage')
 })
 
-app.post('/', function(req, res) {
-  res.send('POST request to the homepage')
+//clientside application sends post request
+app.post('/transaction', function (req, res) {
+  res.send('POST request to the homepage');
+  //load the fabric config
+  var client = Client.loadFromConfig('test/fixtures/network.yaml');
+  //set up the stores
+  client.initCredentialStores()
+    .then((nothing) => {});
+  //new transaction id is created
+  let tx_id = client.newTransactionID(true);
+
+  let request = {
+    config: config,
+    data: req.body
+  };
+  //create channel and instantiate the chaincode
+  var channel = client.createChannel(request);
+  channel.instantiateChaincode();
+  //send a transaction proposal, receive endorsement and return the successful transaction
+  try {
+    res.send(channel.sendTransaction(channel.sendTransactionProposal(request)));
+
+  } catch (ex) {
+    throw ex;
+  }
+
 })
 
 app.listen(port, () => {
